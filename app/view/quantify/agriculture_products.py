e58d491b9ef7100d95903e3e6f0d8d501e3fbdd0
zhandong <1468767640@qq.com>
2018-02-20 17:47:18 +0800

    add

--- app/view/quantify/agriculture_products.py
+++ app/view/quantify/agriculture_products.py
@@ -3,7 +3,8 @@ from app import db
 from . import quantify_blueprint
 from app.model.quantify import Country
 from app.model.comm.log import ArgLog
-from app.model.quantify.agriculture_products import AgricultureIndexes,AgricultureKind,AgricultureFacts,AgricultureTable
+from app.model.quantify.agriculture_products import AgricultureIndexes, AgricultureKind, AgricultureFacts, \
+    AgricultureTable
 from app.model.user import Permission
 from flask_login import current_user
 from app import std_json
@@ -13,17 +14,20 @@ import datetime
 
 ALLOW_ARGS = (
     "tablename",
+    "table_id",
     "country",
     "index",
     "start_time",
     "end_time",
     "batch",
-    "kind"
+    "country_ids",
+    "index_ids",
+    "log_id",
+    "kind_ids"
 )
 
 
-def check_args(father,son):
-
+def check_args(father, son):
     return set(father) > set(son)
 
 
@@ -35,7 +39,7 @@ def find_in_list(items, id, func):
     return None
 
 
-@quantify_blueprint.route("/agriculture_facts", methods=['GET', 'POST','PUT', 'DELETE'])
+@quantify_blueprint.route("/agriculture_facts", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def agriculture_facts():
     if request.method == "GET":
 
@@ -50,7 +54,7 @@ def agriculture_facts():
         start_time = args.get('start_time')
         end_time = args.get('end_time')
 
-        table = AgricultureTable.query.filter_by(id = args.get('table_id')).first()
+        table = AgricultureTable.query.filter_by(id=args.get('table_id')).first()
         if table is None:
             return jsonify(status="fail", data=[], reason="no such table")
 
@@ -62,17 +66,17 @@ def agriculture_facts():
         for country_id in args.get("country_ids"):
             for index_id in args.get("index_ids"):
                 for kind_id in args.get('kind_ids'):
-                    facts = AgricultureFacts.find(table_id=table.id, index_ids=[index_id],kind_id=kind_id,
-                                                country_ids=[country_id], start_time=int(start_time),
-                                                end_time=int(end_time),log_id=log_id)
+                    facts = AgricultureFacts.find(table_id=table.id, index_ids=[index_id], kind_id=kind_id,
+                                                  country_ids=[country_id], start_time=int(start_time),
+                                                  end_time=int(end_time), log_id=log_id)
                     index = AgricultureIndexes.query.filter_by(id=index_id).first()
                     country = Country.query.filter_by(id=country_id).first()
                     kind = AgricultureKind.query.filter_by(id=kind_id).first()
                     result.append(
-                        {"country":country.to_json(),
+                        {"country": country.to_json(),
                          "index": index.to_json(),
                          "kind": kind.to_json(),
-                         "data":[fact.to_json() for fact in facts]})
+                         "data": [fact.to_json() for fact in facts]})
 
         # facts = AgricultureFacts.find(table_id=args.get("table_id"), index=args.get("index"),
         #                                   country=args.get("country"), start_time=args.get("start_time"))
@@ -179,7 +183,8 @@ def agriculture_facts():
     #
     #     return jsonify(status="success", reason="", data=[f.to_json() for f in deleted_facts])
 
-@quantify_blueprint.route("/agriculture_facts/batch", methods=['GET', 'POST','PUT', 'DELETE'])
+
+@quantify_blueprint.route("/agriculture_facts/batch", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def agriculture_facts_batch():
     if request.method == "POST":
         """
@@ -201,7 +206,6 @@ def agriculture_facts_batch():
         note = body.get("note")
         table_id = body.get("table_id")
 
-
         table = AgricultureTable.query.filter_by(id=table_id).first()
         if table is None:
             return jsonify(status="fail", reason="no id table", data=[])
@@ -210,7 +214,7 @@ def agriculture_facts_batch():
 
         old_log = table.get_newest_log()
         new_log = ArgLog(note=note, user_id=current_user.id,
-                     table_id=table_id, timestamp=datetime.now())
+                         table_id=table_id, timestamp=datetime.now())
         db.session.add(new_log)
         db.session.commit()
 
@@ -225,17 +229,16 @@ def agriculture_facts_batch():
                 elif field == "log_id":
                     f.log_id = new_log.id
                 elif field == "time_stamp":
-                   f.time_stamp = datetime.now()
+                    f.time_stamp = datetime.now()
                 else:
                     setattr(f, field, getattr(fact, field))
             db.session.add(f)
             db.session.commit()
 
-
         for data in datas:
             pre_fact = AgricultureFacts.find_one(table_id=table_id, index_id=data.get("index_id"),
-                                                   country_id = data.get('country_id'),kind_id=data.get('kind_id'),
-                                                   time=data.get('time'), log_id=new_log.id)
+                                                 country_id=data.get('country_id'), kind_id=data.get('kind_id'),
+                                                 time=data.get('time'), log_id=new_log.id)
             if pre_fact is not None:
 
                 if data.get("value") is not None:
@@ -266,10 +269,10 @@ def agriculture_facts_batch():
         db.session.add(table)
         db.session.commit()
 
-        return jsonify(status="success",)
+        return jsonify(status="success", )
 
 
-@quantify_blueprint.route("/agriculture_table/<id>/indexes", methods=['GET', 'POST','PUT', 'DELETE'])
+@quantify_blueprint.route("/agriculture_table/<id>/indexes", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def socioeconomic_facts_indexes(id):
     if request.method == "GET":
         table = AgricultureTable.query.filter_by(id=id).first()
@@ -279,7 +282,8 @@ def socioeconomic_facts_indexes(id):
 
         return jsonify(status="fail", reason="no such id table", data=[i.to_json() for i in table.indexes])
 
-@quantify_blueprint.route("/agriculture_table", methods=['GET', 'POST','PUT', 'DELETE'])
+
+@quantify_blueprint.route("/agriculture_table", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def agriculture_table():
     if request.method == "GET":
         tables = AgricultureTable.query.all()
@@ -287,8 +291,8 @@ def agriculture_table():
 
     if request.method == "POST":
         table = AgricultureTable(name=request.form.get("name"),
-                                   cn_alis=request.form.get("cn_alis"),
-                                   en_alis=request.form.get("en_alis"))
+                                 cn_alis=request.form.get("cn_alis"),
+                                 en_alis=request.form.get("en_alis"))
         db.session.add(table)
         db.session.commit()
 
@@ -303,7 +307,6 @@ def agriculture_table():
         return jsonify(status="success", reason="", data=[table.to_json()])
 
     if request.method == "DELETE":
-
         table = AgricultureTable.query.filter_by(id=request.form.get("id")).first()
         db.session.delete(table)
         db.session.commit()
@@ -319,7 +322,7 @@ def agriculture_table():
         return jsonify(status="success", reason="", data=[table.to_json()])
 
 
-@quantify_blueprint.route("/agriculture_index", methods=['GET', 'POST','PUT', 'DELETE'])
+@quantify_blueprint.route("/agriculture_index", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def agriculture_index():
     if request.method == "GET":
         indexes = AgricultureIndexes.query.all()
@@ -327,10 +330,10 @@ def agriculture_index():
 
     if request.method == "POST":
         index = AgricultureIndexes(name=request.form.get("name"),
-                                     cn_alis=request.form.get("cn_alis"),
-                                     en_alis=request.form.get("en_alis"),
-                                     unit=request.form.get("unit"),
-                                     table_id=request.form.get("table_id"))
+                                   cn_alis=request.form.get("cn_alis"),
+                                   en_alis=request.form.get("en_alis"),
+                                   unit=request.form.get("unit"),
+                                   table_id=request.form.get("table_id"))
         db.session.add(index)
         db.session.commit()
 
@@ -348,23 +351,22 @@ def agriculture_index():
         return jsonify(status="success", reason="", data=[index.to_json()])
 
     if request.method == "DELETE":
-
         index = AgricultureIndexes.query.filter_by(id=request.form.get("id")).first()
         db.session.delete(index)
         db.session.commit()
         return jsonify(status="success", reason="", data=[index.to_json()])
 
+
 @quantify_blueprint.route("/agriculture_kinds", methods=['GET', 'POST', 'DELETE'])
 def route_country():
-
     if request.method == "GET":
         kinds = AgricultureKind.query.all()
         return jsonify(status="success", reason="", data=[t.to_json() for t in kinds])
 
     if request.method == "POST":
         kind = AgricultureKind(name=request.form.get("name"),
-                          cn_alias=request.form.get("cn_alis"),
-                          en_alias=request.form.get("en_alis"))
+                               cn_alias=request.form.get("cn_alis"),
+                               en_alias=request.form.get("en_alis"))
 
         return jsonify(status="success", reason="", data=[kind.to_json()])
 
@@ -373,3 +375,56 @@ def route_country():
         db.session.delete(kind)
         db.session.commit()
         return jsonify(status="success", reason="", data=[kind.to_json()])
+
+
+@quantify_blueprint.route("/socioeconomic_facts/graph", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def socioeconomic_facts_graph():
+    if request.method == "GET":
+
+        # if not current_user.can(Permission.QUANTIFY_R):
+        #     return jsonify(status="fail", data=[], reason="no permission")
+
+        args = std_json(request.args)
+
+        if not check_args(ALLOW_ARGS, args.keys()):
+            return jsonify(status="fail", reason="error args", data=[])
+
+        table = AgricultureTable.query.filter_by(id=args.get("table_id")).first()
+        if table is None:
+            return jsonify(status="fail", data=[], reason="no such table")
+
+        log_id = args.get("log_id")
+
+        if log_id is None:
+            old_log = table.get_newest_log()
+            log_id = old_log.id
+
+        start_time = args.get("start_time")
+        # if start_time is not None:
+        #     start_time = datetime.strptime(str(start_time), "%Y")
+
+        end_time = args.get("end_time")
+        # if end_time is not None:
+        #     end_time = datetime.strptime(str(end_time), "%Y")
+        datas = []
+        for index_id in args.get("index_ids"):
+            for country_id in args.get("country_ids"):
+                for kind_id in args.get("kind_ids"):
+                    index = AgricultureIndexes.query.filter_by(id=index_id).first()
+                    kind = AgricultureKind.query.filter_by(id=kind_id).first()
+                    country = Country.query.filter_by(id=country_id).first()
+                    facts = AgricultureFacts.find(table_id=args.get("table_id"), index_ids=[index_id], kind_id=kind_id,
+                                                  country_ids=[country_id], start_time=int(start_time),
+                                                  end_time=int(end_time), log_id=log_id)
+                    data = {
+                        'index': index.to_json(),
+                        'kind': kind.to_json(),
+                        'country': country.to_json(),
+                        'series': [i.to_json() for i in facts]
+                    }
+                    datas.append(data)
+        return jsonify({
+            'status': 'success',
+            'reason': '',
+            'data': datas
+        })
