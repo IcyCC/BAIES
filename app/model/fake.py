16406cf6c2af7fc90ceebca3dcb1c229e193b266
zhandong <1468767640@qq.com>
2018-02-21 15:36:32 +0800

    add

--- app/model/fake.py
+++ app/model/fake.py
@@ -1,6 +1,7 @@
 from app.model.qualitative.information import *
 from app import db
 from app.model.quantify.socioeconomic import *
+from app.model.quantify.agriculture_products import *
 from app.model.comm.log import *
 from app.model.user import *
 
@@ -73,6 +74,79 @@ def insert_post(count=100):
             print("EXCEPETION")
             db.session.rollback()
 
+def insert_agriculture_kinds():
+    fake = Faker(locale='en_AU')
+    i=0
+    while i < 4:
+        index = AgricultureKind(name=fake.name())
+        db.session.add(index)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            print("EXCEPETION")
+            db.session.rollback()
+
+def insert_agriculture_tables():
+    fake = Faker(locale='zh_CN')
+    i=0
+    while i<4:
+        table = AgricultureTable(name=fake.name(), cn_alis=fake.sentence(), en_alis=fake.sentence())
+        db.session.add(table)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            print("EXCEPETION")
+            db.session.rollback()
+
+def insert_agriculture_indexs():
+    t_count = AgricultureTable.query.count()
+    names = ['增长率','下降率','饱和率','平均生产总值']
+    i=0
+    while i<20:
+        t = AgricultureTable.query.offset(randint(0, t_count - 1)).first()
+        index = AgricultureIndexes(
+            name = names[randint(0, 3)],
+            table_id=t.id
+        )
+        db.session.add(index)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            print("EXCEPETION")
+            db.session.rollback()
+
+
+def insert_agriculture_facts():
+    c_count = Country.query.count()
+    k_count = AgricultureKind.query.count()
+    i_count = AgricultureIndexes.query.count()
+    times = [str(i) for i in range(2000,2005)]
+    a=0
+    while a<1000:
+        c = Country.query.offset(randint(0, c_count - 1)).first()
+        k = AgricultureKind.query.offset(randint(0, k_count - 1)).first()
+        i = AgricultureIndexes.query.offset(randint(0, i_count - 1)).first()
+        fact = AgricultureFacts(
+            time = times[randint(0,4)],
+            country_id=c.id,
+            index_id=i.id,
+            kind_id=k.id,
+            log_id=1
+        )
+        db.session.add(fact)
+        try:
+            db.session.commit()
+            a += 1
+        except IntegrityError:
+            print("EXCEPETION")
+            db.session.rollback()
+
+
+
+
 
 
 def insert_test_soc_data():
@@ -135,3 +209,8 @@ def gen():
     insert_country()
     insert_post()
     insert_test_soc_data()
+    insert_kind()
+    insert_agriculture_kinds()
+    insert_agriculture_tables()
+    insert_agriculture_indexs()
+    insert_agriculture_facts()
